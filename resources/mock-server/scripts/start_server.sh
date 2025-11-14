ce9b881093db1b1cc48adbe5fdf3244c8223161f
Joe Polny <joepolny@gmail.com>
2025-11-14 12:36:22 -0500

    feat: scripts/start_server.sh (#9)
    * feat: start_server.sh
    
    * docs: add mock-server README

---
+++ resources/mock-server/scripts/start_server.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DOCKER_DIR=$(dirname "$0")
+
+# Stop and remove any existing container with the same name
+docker rm --force mock-$1-instance 2>/dev/null || true
+
+# Select port based on the arg
+if [ "$1" == "algod" ]; then
+    PORT=8000
+elif [ "$1" == "kmd" ]; then
+    PORT=8001
+elif [ "$1" == "indexer" ]; then
+    PORT=8002
+else
+    echo "Usage: $0 {algod|kmd|indexer}"
+    exit 1
+fi
+
+docker buildx build -t mock-server $DOCKER_DIR/..
+docker run --rm -p $PORT:$PORT --name mock-$1-instance mock-server $1
